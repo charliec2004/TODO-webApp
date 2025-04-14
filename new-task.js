4e19ab162359390cf33455cd2c8dfd1b6522aa30
charliec2004 <charlieconner04@gmail.com>
2025-04-14 15:28:31 +1000

    Created the modal window and closing functionality

--- new-task.js
+++ new-task.js
@@ -1,12 +1,28 @@
 const openNewTask = document.getElementById('open-modal');
 const closeNewTask = document.getElementById('close-modal');
 const modal = document.getElementById('new-task-modal');
+const cancelTask = document.getElementById('cancel-task');
 
-openNewTask.addEventListener("click", () => {
-    modal.classList.add("open")
-})
+// Open the modal
+openNewTask.addEventListener('click', () => {
+    modal.classList.add("open");
+});
 
-closeNewTask.addEventListener("click", () => {
-    modal.classList.remove("open")
-})
+// Click the X button
+closeNewTask.addEventListener('click', () => {
+    modal.classList.remove("open");
+});
+
+// Click outside the modal
+window.addEventListener('click', (e) => {
+    if (e.target === modal) {
+        modal.classList.remove('open'); // 'modal' is the background .. 'modal-content' is the stuff
+    }
+});
+
+// Click Cancel
+cancelTask.addEventListener('click', () => {
+    modal.classList.remove('open');
+});
+
 
